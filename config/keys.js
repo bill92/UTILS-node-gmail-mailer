f11b68176354837a736d1313c976088a13b26eaa
Bill92 <bill92@github.com#>
2020-09-24 17:57:26 +0100

    Untracked files

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  myEmail: 'billymccord92@gmail.com',
-  googleClientId: '68399599437-b38mo627dbhsub2khiq3v01mougot51s.apps.googleusercontent.com',
-  googleClientSecret: 'M81poptIkqbtjYi6T4rBoKaE',
-  googleRedirectURI: 'https://developers.google.com/oauthplayground',
-  refreshToken: '1//048y_yT9dM4zTCgYIARAAGAQSNwF-L9Irb2zKI7yT_9S086nL4zxmJ99ypDshwpORR0vn6t0wkk3j9CB9Y1kQ9L6f7B-5eykJPE0'
-}
